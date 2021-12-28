193429ae33c5f3c9eb5ac5d4ac6aa4a711f66fce
Reginald Wong <reginald_wong@outlook.com>
2021-12-28 16:45:14 -0500

    add form styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
 import './App.css';
-import InputField from "./components/InputField"
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { Todo } from "./components/model";
 
 const App: React.FC = () => {
-
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  return <div className="App">
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), todo, isDone: false }])  
+      setTodo("");
+    } 
+  };
+
+  console.log(todos);
+
+  return (
+    <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
-    </div>;
-  
-}
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
+     
+    </div>
+  );
+};
 
 export default App;
